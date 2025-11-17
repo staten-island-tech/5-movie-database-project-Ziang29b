329ef07bdf5613e524614215e5561cfd99d2ee84
Ziang29b <Ziangc2@nycstudents.net>
2025-11-17 06:25:26 -0800

    Update import time.py

--- import time.py
+++ import time.py
@@ -1,11 +1,15 @@
-genre_filter = input
-user_input= input
-year_filter = input
-user_input= input 
+movies= [movies.json]
+
+genre_filter = input(" genre  (or leave blank): ").lower()
+year_filter = input("year to filter (or leave blank): ")
+year_filter = int(year_filter) if year_filter else None
+title_filter = input(" title (or leave blank) ").lower()
 
 filtered_movies = [
-    movie for movie in movies:
-    if movie["genre"] == genre_filter and movie["year"] >= year_filter
+    movie for movie in movies
+    if (not genre_filter or movie["genre"].lower() == genre_filter)
+    and (not year_filter or movie["year"] >= year_filter)
+    and (not title_filter or movie["title"].lower()==title_filter )
 ]
 
-print(filtered_movies)
+print("Filtered movies:", filtered_movies)
